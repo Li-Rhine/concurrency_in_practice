9847f2f0068cedfe2eb0719a802e1438778c0278
dahai.li <dahai.li@china.zhaogang.com>
2019-11-22 16:39:44 +0800

    中断线程的正确方式 interrupt

--- src/threadcoreknowledge/stopthreads/RightWayStopThreadWithSleepEveryLoop.java
+++ src/threadcoreknowledge/stopthreads/RightWayStopThreadWithSleepEveryLoop.java
@@ -1,23 +1,24 @@
 package threadcoreknowledge.stopthreads;
 
 /**
- * @Description：带有sleep的中断线程写法
- * @Author： Rhine
- * @Date： 2019/11/22 2:02
- **/
-public class RightWayStopThreadWithSleep {
+ * @Author: dahai.li
+ * @Description: 如果在执行过程中，每次循环都会调用sleep或者wait等方法，那么不需要每次迭代都检查是否以中断 !Thread.currentThread().isInterrupted()的判断条件
+ * @Date: Create in 16:31 2019/11/22
+ */
+public class RightWayStopThreadWithSleepEveryLoop {
     public static void main(String[] args) throws InterruptedException {
         Runnable runnable = ()->{
             int num = 0;
 
             try {
-                while (num <= 300 && !Thread.currentThread().isInterrupted()) {
+                while (num <= 10000 && !Thread.currentThread().isInterrupted()) {
                     if (num % 100 == 0 ) {
                         System.out.println(num + "是100的倍数");
                     }
                     num++;
+                    Thread.sleep(10);
                 }
-                Thread.sleep(1000);
+
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -25,7 +26,7 @@ public class RightWayStopThreadWithSleep {
 
         Thread thread = new Thread(runnable);
         thread.start();
-        Thread.sleep(500);
+        Thread.sleep(5000);
         //阻塞状态interrupt会报异常
         thread.interrupt();
     }
