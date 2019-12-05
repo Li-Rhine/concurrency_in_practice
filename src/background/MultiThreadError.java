973f1cb19d73caf2dc3c042badf60e4b52edb4ed
Li-Rhine <39134469+Li-Rhine@users.noreply.github.com>
2019-12-05 23:47:19 +0800

    死锁发生（实现）

---
+++ src/background/MultiThreadError.java
@@ -0,0 +1,64 @@
+package background;
+
+/**
+ * @Description： 第二章线程安全问题，演示死锁
+ * @Author： Rhine
+ * @Date： 2019/12/5 23:27
+ **/
+public class MultiThreadError implements Runnable{
+
+    int flag = 1;
+
+    static Object o1 = new Object();
+    static Object o2 = new Object();
+
+    public static void main(String[] args) {
+
+        MultiThreadError r1 = new MultiThreadError();
+        MultiThreadError r2 = new MultiThreadError();
+
+        r1.flag = 1;
+        r2.flag = 0;
+
+        new Thread(r1).start();
+        new Thread(r2).start();
+    }
+
+
+
+    @Override
+    public void run() {
+        System.out.println("flag = "+ flag);
+        if (flag == 1) {
+            synchronized (o1) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                synchronized (o2) {
+                    System.out.println("1");
+                }
+
+            }
+        }
+
+
+        if (flag == 0) {
+            synchronized (o2) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                synchronized (o1) {
+                    System.out.println("0");
+                }
+
+            }
+        }
+
+    }
+}
